c6b07c289edf61096e93c4bad64fac0c2814ee86
ninjafiveo <mjsekol@live.com>
2024-09-10 12:52:43 -0400

    Add project files.

---
+++ ClassesHaydenCameron2425/PlayerAttributes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesHaydenCameron2425
+{
+    public class PlayerAttributes
+    {
+        //properties
+        public string Name {  get; set; }
+        public string Description {  get; set; }
+        public int Strength {  get; set; }
+
+        //Constructor 
+
+        public void teacher(string name, string description, int strength)
+
+        {
+            Name = name;
+            Description = description;
+            Strength = strength;
+        }
+
+        // Method
+        public void DoSomethingMethod()
+        {
+
+            Console.WriteLine("Your character: ");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Strength: {Strength}");
+        }
+
+    }
+}
---
+++ ClassesHaydenCameron2425/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesHaydenCameron2425
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            PlayerAttributes Sekol = new PlayerAttributes();
+            
+
+            Sekol.Name = "Ninja";
+            Sekol.Description = "I am Mr. Sekol";
+            Sekol.Strength = 1000;
+            Console.WriteLine(Sekol.Name);
+            Console.WriteLine(Sekol.Description);
+            Console.WriteLine(Sekol.Strength);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
